82325e5f64ac8948df5f4277ec8eeb77ecdcf012
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2023-10-30 08:24:17 +0300

    Contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
-    {
-        return $this->render('contact/contact.html.twig', []);
+    public function index(MailerInterface $mailer, HttpFoundationRequest $request, EntityManagerInterface $manager): Response
+    {   
+        $contact = new Contact();
+        $form= $this->createForm(ContactType::class, $contact);
+        $form ->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form ->isValid()) {
+            $contact = $form ->getData();
+            $manager ->persist($contact);
+            $manager->flush();
+
+            $email = (new Email())
+            ->from($contact-> getContactEmail())
+            ->to('admin@example.com')
+            ->subject($contact->getContactSujet())
+            ->text($contact->getContactMessage());
+
+        $mailer->send($email);
+        }
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form
+        ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Contact_nom')
+            ->add('Contact_email', EmailType::class)
+            ->add('Contact_sujet')
+            ->add('Contact_message', TextareaType::class)
+            ->add('Envoyer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
