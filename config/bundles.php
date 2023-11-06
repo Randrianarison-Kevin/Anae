9dacbdd3509d26b5aefd3e7a24c378d55daf3b7e
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2023-11-06 16:01:02 +0300

    Competence_front

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
 ];
--- src/Controller/Admin/CompetenceCrudController.php
+++ src/Controller/Admin/CompetenceCrudController.php
@@ -3,10 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Competence;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Form\Type\TextEditorType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class CompetenceCrudController extends AbstractCrudController
 {
@@ -15,13 +18,13 @@ class CompetenceCrudController extends AbstractCrudController
         return Competence::class;
     }
 
-    
+
     public function configureFields(string $pageName): iterable
     {
         
            
         yield TextField::new('competence_nom');
-        yield TextareaField::new('competence_contenu');
+        yield TextEditorField::new('competence_contenu');
         
     }
    
